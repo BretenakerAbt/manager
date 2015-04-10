e64150f2c1aa4a265a27536ed16d02f47ac6c99f
Benjamin Bretenaker <bretenaker@hotmail.fr>
2015-04-10 15:11:54 +0200

    CodeError
    AssertionError

--- main.py
+++ main.py
@@ -20,12 +20,121 @@ manager - A screen manager for CEToolbox
 import kivy
 
 from kivy.app import App
+from time import time
+from os.path import dirname, join
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty,\
+    ListProperty
 from kivy.uix.widget import Widget
+from kivy.uix.scrollview import ScrollView
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class ManagerScreen(Screen):
+    fullscreen = BooleanProperty(False)
+    
+    def add_widget(self, *args):
+        if 'content' in self.ids:
+            return self.ids.content.add_widget(*args)
+        return super(ManagerApp, self).add_widget(*args)
 
 class ManagerApp(App):
-    pass
+    
+    index = NumericProperty(-1)
+    current_title = StringProperty()
+    time = NumericProperty(0)
+    show_sourcecode = BooleanProperty(False)
+    sourcecode = StringProperty()
+    screen_names = ListProperty([])
+    higherarchy = ListProperty([])
+    
+    def build(self):
+        self.title = 'Hello world'
+        Clock.schedule_interval(self._update_clock, 1 / 60.)
+        self.screens = {}
+        self.available_screens = sorted([
+            'About', 'Modeexpert', 'Modesimple'])
+        self.screen_names = self.available_screens
+        curdir = dirname(__file__)
+        self.available_screens = [join(curdir, 'screens',
+            '{}.kv'.format(fn)) for fn in self.available_screens]
+        self.go_next_screen()
+        
+    def on_pause(self):
+        return True
+
+    def on_resume(self):
+        pass
+        
+    def on_current_title(self, instance, value):
+        self.root.ids.spnr.text = value
+        
+    def go_previous_screen(self):
+        self.index = (self.index - 1) % len(self.available_screens)
+        screen = self.load_screen(self.index)
+        sm = self.root.ids.sm
+        sm.switch_to(screen, direction='right')
+        self.current_title = screen.name
+        self.update_sourcecode()
+        
+    def go_next_screen(self):
+        self.index = (self.index + 1) % len(self.available_screens)
+        screen = self.load_screen(self.index)
+        sm = self.root.ids.sm
+        sm.switch_to(screen, direction='left')
+        self.current_title = screen.name
+        self.update_sourcecode()
+        
+    def go_screen(self, idx):
+        self.index = idx
+        self.root.ids.sm.switch_to(self.load_screen(idx), direction='left')
+        self.update_sourcecode()
+        
+    def go_higherarchy_previous(self):
+        ahr = self.higherarchy
+        if len(ahr) == 1:
+            return
+        if ahr:
+            ahr.pop()
+        if ahr:
+            idx = ahr.pop()
+            self.go_screen(idx)
+            
+    def load_screen(self, index):
+        if index in self.screens:
+            return self.screens[index]
+        screen = Builder.load_file(self.available_screens[index].lower())
+        self.screens[index] = screen
+        return screen
+    
+    def toggle_source_code(self):
+        self.show_sourcecode = not self.show_sourcecode
+        if self.show_sourcecode:
+            height = self.root.height * .3
+        else:
+            height = 0
+
+        Animation(height=height, d=.3, t='out_quart').start(
+                self.root.ids.sv)
+
+        self.update_sourcecode()
+    
+    def read_sourcecode(self):
+        fn = self.available_screens[self.index].lower()
+        with open(fn) as fd:
+            return fd.read()
+        
+    def update_sourcecode(self):
+        if not self.show_sourcecode:
+            self.root.ids.sourcecode.focus = False
+            return
+        self.root.ids.sourcecode.text = self.read_sourcecode()
+        self.root.ids.sv.scroll_y = 1
+        
+    def _update_clock(self, dt):
+        self.time = time()
 
 if __name__ == '__main__':
     ManagerApp().run()
